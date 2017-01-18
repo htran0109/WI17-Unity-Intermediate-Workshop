0f9ca1b1b39a3d0d22339331c07d2270819e2388
Timothy Wang <wangtim1996@gmail.com>
2017-01-17 16:48:26 -0800

    starting explosion, need sound effect / particle system tuning

---
+++ Assets/Scripts/DestoryParticleDone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryParticleDone : MonoBehaviour {
+    private ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+        ps = gameObject.GetComponent<ParticleSystem>();
+        Destroy(gameObject, ps.duration);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ExplodeAfterTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeAfterTime : MonoBehaviour {
+
+    public GameObject explosion;
+
+    public float fusetime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SetFuse()
+    {
+        yield return new WaitForSeconds(fusetime);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+
+    }
+}
